037ee39dc34e18be3de8566b947caff34ff4100a
arash-jj <arshjfry66@gmail.com>
2026-02-26 20:58:16 +0330

    project setup

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Roboto } from 'next/font/google'
+import "./globals.css";
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: "RepoVitals",
+  description: "A tool to analyze and visualize GitHub repositories.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={roboto.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
